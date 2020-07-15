7e62728969570b15920a77ab48f2438614c717b1
Amera AL KTABE <aalktabe@enseirb-matmeca.fr>
2020-07-15 16:15:57 +0200

    Code de base

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/navBar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar className="NavBar"/>
+    <hr />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class navBar extends React.Component {
+    render(){
+    return(
+        <div className="logo">
+            <img className="enseirbLogo" src="./img/enseirb-logo.png" ></img>
+            <div className="txt">
+                <h2 className="txt-1"> Forum Ingénib </h2>
+                <h3 className="txt-2"> 10/10/2020 </h3>
+            </div>
+            <img className="forumLogo" src="./img/forum-logo.png" ></img>
+
+        </div>
+    )
+    }
+}
+
+export default navBar;
